c36f0a0c75941e503d2d539dd0a116ddb077281f
vuvandang1995 <dangdiendao@gmail.com>
2018-04-14 15:34:43 +0700

    ok

--- osticket_v1/user/views.py
+++ osticket_v1/user/views.py
@@ -135,9 +135,10 @@ def activate(request, uidb64, token):
     if user is not None and account_activation_token.check_token(user, token):
         user.status = 1
         user.save()
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
         mess = 'Thank you for your email confirmation. Now you can login your account.'
         # return render(request, 'user/index.html',{'mess': mess})
-        return redirect('/', {'mess': mess})
+        # return redirect('/', {'mess': mess})
     else:
         return HttpResponse('Activation link is invalid!')
 
