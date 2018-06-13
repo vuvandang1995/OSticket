877085229e7534ff49cc7ad1d678b557dcb969e2
Vũ Văn Đặng <dangdiendao@gmail.com>
2018-06-13 08:51:28 +0700

    update code member

--- osticket_chat_template/user/static/js/lib/calendar-2/pignose.init.js
+++ osticket_chat_template/user/static/js/lib/calendar-2/pignose.init.js
@@ -1,11 +1,31 @@
 $(function() {
     "use strict";
     $('.year-calendar').pignoseCalendar({
-        theme: 'blue' // light, dark, blue
+        theme: 'blue', // light, dark, blue
+        select: function(date, context) {
+            // var token = $("input[name=csrfmiddlewaretoken]").val();
+            // $.ajax({
+            //     type:'POST',
+            //     url:location.href,
+            //     data: {'date': JSON.stringify(date[0]).split('T')[0].substring(1), 'csrfmiddlewaretoken':token},
+            //     success: function(){
+            //         setTimeout(function(){
+            //             $("body #list_tk_history").load(location.href + " #list_tk_history");
+            //         }, 3000);
+            //     }
+            // });
+
+            group_agent_Socket.send(JSON.stringify({
+                'message' : JSON.stringify(date[0]).split('T')[0].substring(1),
+                'time' : 'date',
+            }));
+        }
     });
 
     $('input.calendar').pignoseCalendar({
         format: 'YYYY-MM-DD' // date format string. (2017-02-02)
     });
+
+    //alert(('body .year-calendar .pignose-calendar-body .pignose-calendar-row .pignose-calendar-unit-date').children('a').html());
 });
 
