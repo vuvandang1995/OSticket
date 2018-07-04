4248d3c056f32a6d9586c523ca3bab71b7452233
dung1101 <mrbeo1221119@gmail.com>
2018-07-04 16:27:10 +0700

    update

--- osticket/user/static/js/manager_topic.js
+++ osticket/user/static/js/manager_topic.js
@@ -45,16 +45,25 @@ $(document).ready(function(){
                 list_agent.push(this.name);
             }
         });
-        $.ajax({
-            type:'POST',
-            url:location.href,
-            data: {'add_topic': topicname, 'description': description, 'csrfmiddlewaretoken':token, 'topicid': topicid, 'list_agent[]': JSON.stringify(list_agent), 'department': department},
-            success: function(){
-                // window.location.reload();
-                $("#list_topic").load(location.href + " #list_topic");
-                document.getElementById("add_topic_close").click();
-            }
-        });
+        $("#nameerr").html("");
+        $("#deserr").html("");
+        if (topicname==''){
+            $("#nameerr").html("not null");
+        }
+        else if(description==''){
+            $("#deserr").html("not null");
+        }else{
+            $.ajax({
+                type:'POST',
+                url:location.href,
+                data: {'add_topic': topicname, 'description': description, 'csrfmiddlewaretoken':token, 'topicid': topicid, 'list_agent[]': JSON.stringify(list_agent), 'department': department},
+                success: function(){
+                    // window.location.reload();
+                    $("#list_topic").load(location.href + " #list_topic");
+                    document.getElementById("add_topic_close").click();
+                }
+            });
+        }
     });
 
 
